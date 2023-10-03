d28b3710f9833ed5c355f7ee3996d23e65a5b5b1
lancesahagun <lance.sahagun03@gmail.com>
2023-10-02 23:05:26 -0700

    imported Material UI Library & began work on Home Page
    for A4:
    MUI Documentation: https://mui.com/material-ui/getting-started/
    
    main implementation: button -> test different MUI functions later
    
    reference wireframe to actually design home page later (and update issues/development tracking)

--- frontend/src/pages/home.js
+++ frontend/src/pages/home.js
@@ -1,6 +1,49 @@
 //Temporary, change code below later
-function Home(){
-    return <h1>Home</h1>
-}
 
+//importing react library for react components
+import React from 'react';
+
+//importing MUI library for user interface components
+import { Container, Typography, Button } from '@mui/material';
+import { Article } from '@mui/icons-material';
+
+function Home(){
+    return (
+        //container to hold information (will need to play with/change later)
+        <Container maxWidth="md">
+
+          <Typography variant="h2" align="center" gutterBottom>
+            Home
+          </Typography>
+
+          <Typography variant="body1" align="center" paragraph>
+            Under Construction!
+            <br />
+            Future Plans: 
+            <br />
+            Container Block to separate sections
+            <br />
+            Logo - Images
+            <br />
+            Get Started Button
+            <br />
+            Links to other parts of site
+          </Typography>
+
+        //test component with functionality (button that links to Discord API website)
+          <Button
+            variant="contained"
+            color="primary"
+            startIcon={<Article />}
+            href="https://discord.com/developers/docs/reference"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            View the Discord API Documentation
+          </Button>
+
+        </Container>
+      );
+    }
+    
 export default Home;
