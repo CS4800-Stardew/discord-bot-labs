c156776ca3d6d584b4b9dd04475ebadaec6ba3ad
katherine-co <112907801+katherine-co@users.noreply.github.com>
2023-10-03 09:25:12 -0700

    installed react-bootstrap

--- frontend/src/pages/botBuilder.js
+++ frontend/src/pages/botBuilder.js
@@ -1,6 +1,13 @@
-//Temporary, change code below later
+import Button from 'react-bootstrap/Button'
+import 'bootstrap/dist/css/bootstrap.css'
+
 function BotBuilder(){
-    return <h1>Bot Builder</h1>
+    return (
+        <div>
+            <h1>Bot Builder</h1>
+            <Button variant="primary" size="lg">Build Your Bot</Button>
+        </div>
+    );
 }
 
 export default BotBuilder;
