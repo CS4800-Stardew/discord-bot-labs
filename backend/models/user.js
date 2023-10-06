887ded6b4160308fbf2dd62205358895cb1bbdca
Brian Song <79394580+AlphaMiyaL@users.noreply.github.com>
2023-10-05 22:10:27 -0700

    user model schema for mongodb + test case
    imports the libraries joi, jsonwebtoken, config, and mongoose

---
+++ backend/models/user.js
@@ -0,0 +1,71 @@
+// defines Mongoose schema and model for user data, as well as validation functions for user objects
+
+const config = require("config"); // 'config' module for configuration settings
+const jwt = require("jsonwebtoken"); // library for generating JSON Web Tokens
+const Joi = require("joi"); // library for data validation
+const mongoose = require("mongoose"); // library for defining schemas and models
+
+// Mongoose schema for user data
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    minlength: 2,
+    maxlength: 50
+  },
+  email: {
+    type: String,
+    required: true,
+    minlength: 5,
+    maxlength: 255,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 5,
+    maxlength: 1024
+  },
+  isAdmin: Boolean
+});
+
+// generating an authentication token for the user
+userSchema.methods.generateAuthToken = function() {
+  const token = jwt.sign(
+    {
+      _id: this._id,
+      name: this.name,
+      email: this.email,
+      isAdmin: this.isAdmin
+    },
+    config.get("jwtPrivateKey")
+  );
+  return token;
+};
+
+// creating Mongoose model named 'User' using the 'userSchema'
+const User = mongoose.model("User", userSchema);
+
+// validation function for user objects using Joi
+function validateUser(user) {
+  const schema = {
+    name: Joi.string()
+      .min(2)
+      .max(50)
+      .required(),
+    email: Joi.string()
+      .min(5)
+      .max(255)
+      .required()
+      .email(),
+    password: Joi.string()
+      .min(5)
+      .max(255)
+      .required()
+  };
+
+  return Joi.validate(user, schema);
+}
+
+exports.User = User;
+exports.validate = validateUser;
---
+++ backend/test/user.test.js
@@ -0,0 +1,29 @@
+// tests 'generateAuthToken' method of the User model
+
+const { User } = require('../models/user'); // User model
+const jwt = require('jsonwebtoken'); // library for JWT verification
+const config = require('config'); // 'config' for configuration settings
+const mongoose = require('mongoose'); // library for working with object IDs
+
+// Describe the test case
+describe('user.generateAuthToken', () => {
+  it('should return a valid JWT', () => {
+    // create payload with sample user ID and isAdmin status
+    const payload = { 
+      _id: new mongoose.Types.ObjectId().toHexString(), 
+      isAdmin: true 
+    };
+    
+    // create new User instance with payload
+    const user = new User(payload);
+    
+    // generate authentication token using 'generateAuthToken' method
+    const token = user.generateAuthToken();
+    
+    // Verify generated token using JWT library and app's secret key
+    const decoded = jwt.verify(token, config.get('jwtPrivateKey'));
+    
+    // Expect that decoded token matches original payload
+    expect(decoded).toMatchObject(payload);
+  });
+});
