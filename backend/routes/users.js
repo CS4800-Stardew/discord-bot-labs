95db7e698d91cb1d73399a5d16dfec71bbb2746f
Brian Song <79394580+AlphaMiyaL@users.noreply.github.com>
2023-10-26 10:08:19 -0700

    login fix and register error handling
    -fix joi validation for auth
    -add try catch for users so server does not crash when something goes wrong

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -15,31 +15,36 @@ router.get('/me', auth, async (req, res) => {
 // register new user
 router.post('/', async (req, res) => {
   // validate user data using Joi validation
-   const { error } = validateUser(req.body);
-   if (error) return res.status(400).send(error.details[0].message);
+  const { error } = validateUser(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
 
   // check if user already exists by email
-  let user = await User.findOne({ email: req.body.email });
-  if (user) return res.status(400).send('User already registered.');
+  try {
+    let user = await User.findOne({ email: req.body.email });
+    if (user) return res.status(400).send('User already registered.');
 
-  // create new User instance with selected properties from the request body
-  user = new User(_.pick(req.body, ['email', 'username', 'password']));
+    // create new User instance with selected properties from the request body
+    user = new User(_.pick(req.body, ['email', 'username', 'password']));
 
-  // generate salt and hash user's password
-  const salt = await bcrypt.genSalt(10);
-  user.password = await bcrypt.hash(user.password, salt);
+    // generate salt and hash user's password
+    const salt = await bcrypt.genSalt(10);
+    user.password = await bcrypt.hash(user.password, salt);
 
-  // save new user to database
-  await user.save();
+    // save new user to database
+    await user.save();
 
-  // generate authentication token for user
-  const token = user.generateAuthToken();
+    // generate authentication token for user
+    const token = user.generateAuthToken();
 
-  // set 'x-auth-token' header and send user data (excluding password)
-  res
-    .header('x-auth-token', token)
-    .header('access-control-expose-headers', 'x-auth-token')
-    .send(_.pick(user, ['_id', 'name', 'email']));
+    // set 'x-auth-token' header and send user data (excluding password)
+    res
+      .header('x-auth-token', token)
+      .header('access-control-expose-headers', 'x-auth-token')
+      .send(_.pick(user, ['_id', 'name', 'email']));
+  }
+  catch (error) {
+    console.log(error);
+  }
 });
 
 export default router;
